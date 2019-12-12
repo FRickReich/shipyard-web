d862e55b878704fbce2f5f0b0a423c8acdd1732b
F. Rick Reich <frickreich@gmail.com>
2019-12-12 18:25:32 +0100

    Change JSON output

--- server/routes/api/content.js
+++ server/routes/api/content.js
@@ -2,12 +2,11 @@
 
 module.exports = (app) => {
 	app.get(`/api/:userId/content/message-of-the-day`, (req, res, next) => {
-		res.json({
-			items: [
+		res.json([
 				{ user: req.params.userId },
 				{ success: true },
 				{ message: 'Hello World' }
 			]
-		});
+		);
 	});
 };
