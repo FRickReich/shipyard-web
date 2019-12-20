c20265139469f327b54b9db94a6fc7b4351514b1
F. Rick Reich <frederikreich@googlemail.com>
2019-12-20 17:37:10 +0100

    Update content.js

--- server/routes/api/content.js
+++ server/routes/api/content.js
@@ -1,22 +1,19 @@
 'use strict';
 
-module.exports = (app) =>
-{
-	app.get(`/api/:userId/content/message-of-the-day`, (req, res, next) =>
-	{
+module.exports = (app) => {
+	app.get(`/api/:userId/content/message-of-the-day`, (req, res, next) => {
 		res.json({
-			"version": "1.0",
-			"success": true,
-			"data": {
-				"sampleArray": [
-					"string value",
+			version: '1.0',
+			success: true,
+			data: {
+				sampleArray: [
+					'string value',
 					5,
 					{
-						"name": "sub object"
+						name: 'sub object'
 					}
 				]
 			}
-		}
-		);
+		});
 	});
 };
