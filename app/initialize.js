a66811a2127562a820a5798a1cb5c93eef59ef8d
F. Rick Reich <frickreich@gmail.com>
2020-01-02 09:24:45 +0100

    Update initialize.js

--- app/initialize.js
+++ app/initialize.js
@@ -10,6 +10,12 @@ import NotFound from './scenes/Page/NotFound/NotFound';
 import Register from './scenes/Page/Register/Register';
 
 import DashboardOverview from './scenes/Dashboard/DashboardOverview/DashboardOverview';
+import DashboardSettings from './scenes/Dashboard/DashboardSettings/DashboardSettings';
+import DashboardProjectCreate from './scenes/Dashboard/DashboardProjectCreate/DashboardProjectCreate';
+import DashboardProfile from './scenes/Dashboard/DashboardProfile/DashboardProfile';
+
+import DashboardTranslationCollections from './scenes/Dashboard/DashboardTranslationCollections/DashboardTranslationCollections';
+import DashboardTranslationLanguages from './scenes/Dashboard/DashboardTranslationLanguages/DashboardTranslationLanguages';
 
 ReactDOM.render(
 	<Router>
@@ -18,6 +24,11 @@ ReactDOM.render(
 				<Route exact path="/" component={Home} />
 				<Route exact path="/dashboard/login" component={Login} />
 				<Route exact path="/dashboard" component={DashboardOverview} />
+				<Route exact path="/dashboard/settings" component={DashboardSettings} />
+				<Route exact path="/dashboard/profile" component={DashboardProfile} />
+				<Route exact path="/dashboard/projects/create" component={DashboardProjectCreate} />
+				<Route exact path="/dashboard/translations/collections" component={DashboardTranslationCollections} />
+				<Route exact path="/dashboard/translations/languages" component={DashboardTranslationLanguages} />
 				<Route path="/register" component={Register} />
 				<Route component={NotFound} />
 			</Switch>
